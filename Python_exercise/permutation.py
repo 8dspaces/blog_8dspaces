9a8021a30327073a0bfce6527d650a6256520e5f
8dspaces <mickqi1985@hotmail.com>
2014-10-23 10:55:59 +0800

    Update permutation.py

--- Python_exercise/permutation.py
+++ Python_exercise/permutation.py
@@ -16,3 +16,41 @@ for item in list(perms([1, 2, 3,4])):
 
 import itertools
 print list(itertools.permutations([1, 2, 3,4],3))
+
+
+
+#-------------------
+
+#生成全排列
+def perm(items, n=None):
+    if n is None:
+        n = len(items)
+    for i in range(len(items)):
+        v = items[i:i+1]
+        if n == 1:
+            yield v
+        else:
+            rest = items[:i] + items[i+1:]
+            for p in perm(rest, n-1):
+                yield v + p
+ 
+#生成组合
+def comb(items, n=None):
+    if n is None:
+        n = len(items)    
+    for i in range(len(items)):
+        v = items[i:i+1]
+        if n == 1:
+            yield v
+        else:
+            rest = items[i+1:]
+            for c in comb(rest, n-1):
+                yield v + c
+ 
+a = perm('abc')
+for b in a:
+    print b
+    break
+print '-'*20
+for b in a:
+    print b 
